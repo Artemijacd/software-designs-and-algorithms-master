8c81ce385ef6c66ec32580b832a4cfdd1227f92a
Artemii Iakovlev <artemii.iakovlev@pearson.com>
2022-10-09 13:06:44 +0400

    homework - 1

--- docs/2-building-blocks-of-oop-part-1/hometask/src/Point.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Point.ts
@@ -1,1 +1,37 @@
-export class Point {}
+export class Point {
+    private x: number;
+    private y: number;
+
+    constructor();
+    constructor(x: number, y: number);
+    constructor(x?: number, y?: number) {
+        this.x = x ?? 0;
+        this.y = y ?? 0;
+    };
+
+    toString(): string {
+        return `(${this.x}, ${this.y})`
+    }
+
+    distance(): number;
+    distance(other: Point): number;
+    distance(x: number, y: number): number;
+
+    distance(other?: Point | number, y?: number): number {
+        const formula = (xA,yA) => (xB, yB) => {
+            return Math.sqrt(Math.pow((xB-xA), 2) + Math.pow((yB-yA), 2))
+        }
+        const fromPoint = formula(this.x, this.y);
+        if (typeof other === 'number' && y !== undefined) {
+            return fromPoint(other, y);
+        } 
+        if (other === undefined && y === undefined) {
+            return fromPoint(0,0);
+        } else {
+            const point = other as Point;
+            const { x, y } = point;
+            return fromPoint(x, y);
+        }
+        
+    }
+}
--- docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
@@ -1,3 +1,39 @@
+import { Point } from "./point";
 export abstract class Shape {
+
+  protected points: Array<Point>;
+  protected color: string;
+  protected filled: boolean;
+
+  constructor(points: Array<Point>);
+  constructor(points: Array<Point>, color: string, filled: boolean);
+  
+  constructor(points?: Array<Point>, color?: string, filled?: boolean) {
+    if (points !== undefined) {
+      if (points[2] === undefined) {
+        throw 'not enough points'
+      } else {
+        this.points = points;
+        if (color !== undefined && filled !== undefined){
+          this.color = color;
+          this.filled = filled;
+        } else {
+          this.color = 'green';
+          this.filled = true;
+        }
+      }
+    }
+  }
+
+  toString(): string {
+    return `A Shape with color of ${this.color} and ${this.filled ? '': 'not ' }filled. Points: ${this.points[0]}, ${this.points[1]}, ${this.points[2]}.`
+  }
+
+  getPerimeter(): number {
+    let index = 0;
+    const sum = (acc, point) => (index++, acc + point.distance(this.points[index]));
+    return this.points.reduce(sum, 0);
+  }
+
   abstract getType(): string;
 }
