bd5912817b852d568c83a5dfc817cbd5e0dec996
Artemii Iakovlev <artemii.iakovlev@pearson.com>
2022-10-15 18:17:20 +0400

    homework -1

--- docs/3-building-blocks-of-oop-part-2/hometask/src/Bow.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Bow.ts
@@ -1,1 +1,18 @@
-// your code goes here
+import { Weapon } from "./Weapon";
+export class Bow extends Weapon {
+    constructor(baseDamage: number, baseDurability: number, value: number, weight: number)
+    constructor(baseDamage: number, baseDurability: number, value: number, weight: number) {
+        super('bow', value, weight, baseDamage, baseDurability);
+    }
+
+    public polish(): void {
+
+        if (this.getDurability() / 100 < 1) {
+            this.setDurability();
+            console.log(`Your bow durability now is ${this.getDurability()}.`);
+        } else {
+            console.log(`Your bow reached maximum durability.`);
+        }
+
+    }
+}
--- docs/3-building-blocks-of-oop-part-2/hometask/src/ItemWeightComparator.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/ItemWeightComparator.ts
@@ -2,7 +2,7 @@ import { Item } from './Item';
 import { ItemComparator } from './ItemComparator';
 
 export class ItemWeightComparator implements ItemComparator {
-    public compare(first: Item, second: Item) {
-        // your code goes here
+    public compare(first: Item, second: Item): number {
+        return first.getWeight() - second.getWeight();
     }
 }
