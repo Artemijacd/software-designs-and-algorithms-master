22880321f46d46e2879b9b7048f55d15da1378da
Artemii Iakovlev <artemii.iakovlev@pearson.com>
2022-10-18 12:49:58 +0400

    fixes hw-1

--- docs/2-building-blocks-of-oop-part-1/hometask/src/Point.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Point.ts
@@ -9,29 +9,33 @@ export class Point {
         this.y = y ?? 0;
     };
 
-    toString(): string {
+    public toString(): string {
         return `(${this.x}, ${this.y})`
     }
 
     distance(): number;
     distance(other: Point): number;
-    distance(x: number, y: number): number;
+    distance(x: number, yExt: number): number;
 
-    distance(other?: Point | number, y?: number): number {
-        const formula = (xA,yA) => (xB, yB) => {
+    public distance(other?: Point | number, yExt?: number): number {
+
+        const formula = (xA: number, yA: number) => (xB: number, yB: number) => {
             return Math.sqrt(Math.pow((xB-xA), 2) + Math.pow((yB-yA), 2))
         }
+
         const fromPoint = formula(this.x, this.y);
-        if (typeof other === 'number' && y !== undefined) {
-            return fromPoint(other, y);
+        if (typeof other === 'number' && yExt !== undefined) {
+            return fromPoint(other, yExt);
         } 
-        if (other === undefined && y === undefined) {
+        
+        if (other === undefined && yExt === undefined) {
             return fromPoint(0,0);
-        } else {
-            const point = other as Point;
-            const { x, y } = point;
-            return fromPoint(x, y);
         }
+
+        const point = other as Point;
+        const { x, y } = point;
+        return fromPoint(x, y);
         
     }
+
 }
